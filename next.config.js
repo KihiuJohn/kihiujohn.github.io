ce85be61af5bdd2304ab990a3b7bd22f0c9b9385
John Kihiu <kihiuj3@gmail.com>
2025-03-21 20:20:47 +0300

    c

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['techicons.dev', 'cdn.sanity.io', 'icon.icepanel.io', 'images.unsplash.com'],
   },
-  allowedDevOrigins: [
-    '*.preview.same-app.com',
-  ],
-  output: 'export', // Enables static HTML export for GitHub Pages
+  allowedDevOrigins: ['*.preview.same-app.com'],
 };
-
 module.exports = nextConfig;
